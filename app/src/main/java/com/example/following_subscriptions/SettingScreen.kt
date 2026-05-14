7199aec3afe4f6598da7a6a03181fefa732e7932
Kara July <karamellem15@gmail.com>
2026-05-14 15:03:03 +0300

    Добавка аккаунта

--- app/src/main/java/com/example/following_subscriptions/SettingScreen.kt
+++ app/src/main/java/com/example/following_subscriptions/SettingScreen.kt
@@ -1,25 +1,36 @@
 package com.example.following_subscriptions
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,11 +39,6 @@ import com.example.following_subscriptions.ui.theme.DeepBlue
 import com.example.following_subscriptions.ui.theme.DuricFont
 import com.example.following_subscriptions.ui.theme.LletreFont
 import com.google.firebase.auth.FirebaseAuth
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-
 
 
 @OptIn(ExperimentalStdlibApi:: class)
@@ -103,7 +109,7 @@ fun SettingScreen(){
         )
         Spacer(modifier = Modifier.width(20.dp))
         SettingItem( icon = Icons.Default.Email, title = "Email", value = userEmail)
-        SettingItem( icon = Icons.Default.Lock, title = "Пароль", value = "**")
+        SettingItem( icon = Icons.Default.Lock, title = "Пароль", value = "********")
 
     }
 
@@ -121,10 +127,10 @@ fun SettingItem(icon: ImageVector, title: String, value: String){
         verticalAlignment = Alignment.CenterVertically
     ){
         Icon(imageVector = icon, contentDescription = null, tint = CreamWhite, modifier = Modifier.size(28.dp))
-        Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.width(13.dp))
         Text( text = title, color = CreamWhite, fontSize = 18.sp, fontFamily = DuricFont, modifier = Modifier.weight(1f))
         Text( text = value, color = CreamWhite.copy(alpha = 0.6f), fontSize = 16.sp, fontFamily = DuricFont)
-        Icon(Icons.Default.KeyboardArrowRight, contentDescription = null, tint = CreamWhite.copy(alpha = 0.6f))
+        Icon(Icons.AutoMirrored.Filled.KeyboardArrowRight, contentDescription = null, tint = CreamWhite.copy(alpha = 0.6f))
     }
 }
 
