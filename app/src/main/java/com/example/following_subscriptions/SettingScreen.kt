908d77811b17f86336ad375996ce2b6df789f213
Kara July <karamellem15@gmail.com>
2026-05-17 11:32:59 +0300

    Изменение добавления карточки

--- app/src/main/java/com/example/following_subscriptions/SettingScreen.kt
+++ app/src/main/java/com/example/following_subscriptions/SettingScreen.kt
@@ -19,16 +19,11 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,7 +35,6 @@ import com.example.following_subscriptions.ui.theme.CreamWhite
 import com.example.following_subscriptions.ui.theme.DeepBlue
 import com.example.following_subscriptions.ui.theme.DuricFont
 import com.example.following_subscriptions.ui.theme.LletreFont
-import com.google.firebase.auth.FirebaseAuth
 
 
 @OptIn(ExperimentalStdlibApi::class)
